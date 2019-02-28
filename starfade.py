5cc6f313add04cb453fe6a9191446064c2b19ee2
telnetreps <48070999+telnetreps@users.noreply.github.com>
2019-02-28 07:07:15 +0000

    Update starfade.py

--- starfade.py
+++ starfade.py
@@ -3,74 +3,19 @@ import discord
 import os
 import asyncio 
 import time
-import aiohttp
 import random
 import praw
 import json
 import pyfiglet
-import requests
-from urllib.request import (Request, urlopen, urlretrieve)
-import youtube_dl
 import argparse
-import urllib.request as req
 from os import system
 from discord.utils import get
 
 
 
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn' 
-}
-
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-
 bot = commands.Bot(command_prefix="./")
 
-response = requests.get("https://www.reddit.com/r/MEOW_IRL.json", headers={"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36": "linux:memebot:v1.0.0"})
 
-reddit = praw.Reddit(client_id='X6PjDopgqrQExw',
-                     client_secret='SFhKi_7OM2Fwv1UvedOJGn5fT-4',
-                     user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36')
 
 bot.remove_command('help')
 
